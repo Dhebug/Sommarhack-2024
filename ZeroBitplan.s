a8d968b4d9b0444191b844b06caddacd876eddee
Dbug <mike@defence-force.org>
2024-06-29 19:50:49 +0200

    Got a partially working ff8240.w version of MonoSlide

--- ZeroBitplan.s
+++ ZeroBitplan.s
@@ -25,9 +25,6 @@
 ; MonoSlide (2022):  https://demozoo.org/productions/310182/
 ;
 
-
-enable_proper_setup		equ 1     ; Enables the tabbed out code
-
  opt o+,w+
 
 
@@ -59,8 +56,7 @@ t1 set (\1-t6*6-t5*5-t4*4-t3*3-t2*2)
 ;   Program start
 ; ------------------
 ProgStart 
- ifne enable_proper_setup
-	; We call the main routine in supervisor mode
+ 	; We call the main routine in supervisor mode
 	; and when we come back, we return to the caller 
 	move.l #super_main,-(sp)
 	move.w #$26,-(sp)         ; XBIOS: SUPEXEC
@@ -69,69 +65,97 @@ ProgStart
 
 	clr.w -(sp)               ; GEMDOS: PTERM(0)
 	trap #1
- else
-	; We switch to supervisor, but never come back
-	; so no need to correct the stack pointer
-	clr.l -(sp)
-	move.w #$20,-(sp)  ; GEMDOS: SUPER
-	trap #1
- endc
-	
+ 	
 
 super_main
- lea $ffff8240.w,a6
- move.l #$07770727,$ffff8242.w  ; The White and Purple crosses [Could be 2(a6) but same size]
-
- ifne enable_proper_setup
-	;
+ 	;
 	; Save context
 	;
+	move.w #$2700,sr
 	move.b $fffffa07.w,save_iera
+	sf $fffffa07.w					; iera
+
 	move.b $fffffa09.w,save_ierb
+	sf $fffffa09.w					; ierb
+
 	move.b $ffff8260.w,save_resol
 
-	clr.w -(sp)
-	pea -1.w
-	pea -1.w
-	move.w #5,-(sp)
-	trap #14
-	lea 12(sp),sp
- endc
+ 	move.b $ffff8201.w,save_screen_addr_1	; Screen base pointeur (STF/E)
+ 	move.b $ffff8203.w,save_screen_addr_2	; Screen base pointeur (STF/E)
 
-  
+	move.w $ffff8240.w,save_background
+
+ 	move.l $70.w,save_70					          ; Save original VBL handler
+ 	move.l #VblDoNothing,$70.w
+	move.w #$2300,sr
+
 	;
-	; VSync and disable interrupts
+	; Set the screen at the right adress
 	;
-	move.w #37,-(sp)       ; VSYNC
-	trap #14
- ifne enable_proper_setup
-	addq.l #2,sp
- endc
+	lea $ffff8201.w,a0               	; Screen base pointeur (STF/E)
+	move.l #screen_buffer+256,d0
+	clr.b d0
+	lsr.l #8,d0					        ; Allign adress on a byte boudary for STF compatibility
+	movep.w d0,0(a0) 
+	sf.b 12(a0)					        ; For STE low byte to 0
+
 
 	;
 	; Main loop
 	;
-	move.w #$2700,sr
-loop
-	move.w #37,-(sp)       	 ; Vertical Synchronization (adds a 1/71th of a second delay)
-	trap #14
-	addq.l #2,sp
+	move.l #VblFlipFlop,$70.w
+.loop
+ 	; We do the key check before the synchronisation to avoid wobbly rasters
+ 	cmp.b #$39,$fffffc02.w
+	bne.s .loop
+
+	;
+	; Restore system
+	;
+ 	move.w #$2700,sr
+
+	move.b #0,YmDataAVolume
+	bsr UpdateYM
+
+ 	move.l save_70,$70.w
+
+	move.b save_iera,$fffffa07.w
+	move.b save_ierb,$fffffa09.w
+	move.b save_resol,$ffff8260.w
+
+ 	move.b save_screen_addr_1,$ffff8201.w
+ 	move.b save_screen_addr_2,$ffff8203.w
+
+	move.w save_background,$ffff8240.w
+
+	move.w #$2300,sr
+	rts
+ 
+
+
+
+VblFlipFlop
+	movem.l d0-d7/a0-a6,-(sp)
+
+	lea $ffff8240.w,a6
 
 	move.w #$0,(a6)          ; Black top background
- 
- ; We do the key check before the synchronisation to avoid wobbly rasters
- ifne enable_proper_setup
-	cmp.b #$39,$fffffc02.w
-	beq exit
- endc
 
 	lea Whatever,a0
 
-	move.w #$370,d6        ; Color of first tile
-	move.w #$263,d7        ; Color of second tile
-	move.w #$fff,8(a6)
+	move.w ShiftPosition,d0	
+	subq #1,ShiftCounter
+	bne .skip
+	move #50,ShiftCounter
+	addq.w #2,d0
+	and.w #15,d0
+	move.w d0,ShiftPosition 
+.skip
 
-	;add.w #1,ShiftOffset
+	; Do some weird noise with the YM using the current shift registers
+	move.b d0,YmDataAFrequency
+	move.b d0,YmDataNoiseFrequency
+	bsr UpdateYM
 
 	; Line zero synchronisation
 	moveq #14,d2
@@ -141,43 +165,88 @@ loop
 	sub.b d0,d2
 	lsl.b d2,d0
 
-	;bra MindBender
-	bra MonoSlide
+	;bsr MindBender
+	bsr MonoSlide
 
-Whatever	 dc.l 0  
-ShiftOffset  dc.w 8,8
+	movem.l (sp)+,d0-d7/a0-a6
+VblDoNothing
+	rte
+
+
+UpdateYM
+	; Do some weird noise with the YM using the current shift registers
+	lea YmData(pc),a0
+	moveq #5-1,d0
+.loop_ym
+	move.b (a0)+,$ffff8800.w
+	move.b (a0)+,$ffff8802.w
+	dbra d0,.loop_ym 
+	rts
+
+
+ShiftCounter    dc.w 50
+ShiftPosition 	dc.w 0
 
 MonoSlide
 	move.l #$0000FFFF,d6   ; Color of tile
+	
+	moveq #0,d1
+	moveq #0,d2
+	move ShiftPosition,d1
+	sub.l d1,d2
 
-	move.l ShiftOffset,d5              ; Shifter 
-
-	; 366*3=1098
-	; 274*4=1098
-
-	; To leave room for the "Dbug" line
-	; 64 nops*8 = 
-	;move.w #138-1,d0
-	move.w #157-1,d0
-.delay
-	subq.w #1,8(a6)
-	dbra d0,.delay    ; 3
+	; Complete the top black line (+ offset to show the start)
+	pause 45-10
 
 	; The alternated color grid
 	; d0 - trash register used by the "pause" macro and the various delays
-	; d1 - sections counter
-	; d2 - squares counter
-	; d3 - alternate counter
-	; d4
-	; d5
+	; d1 
+	; d2 
+	; d3
+	; d4 - Contains the shifting offset increment
+	; d5 - Contains the shifting offset
 	; d6 - Contains the color of the tiles (each of the words has a different color)
-	; d7
-	moveq #3-1,d3
-.loop_alternate	
-	moveq #3-1,d1 
-.loop_lines
-	; 247
-	move.w #16-1,d2
+	; d7 - Line counter
+
+	; One direction
+	moveq #0,d5
+	bsr DrawBlackAndWhiteTiles
+	move.l d1,d5
+	bsr DrawBlackAndWhiteTiles
+	nop
+	bsr DrawBlackAndWhiteTiles
+	move.l d2,d5
+	bsr DrawBlackAndWhiteTiles
+	nop
+	bsr DrawBlackAndWhiteTiles
+	move.l d1,d5
+	bsr DrawBlackAndWhiteTiles
+	nop
+	bsr DrawBlackAndWhiteTiles
+	move.l d2,d5
+	bsr DrawBlackAndWhiteTiles
+	nop
+	bsr DrawBlackAndWhiteTiles
+	nop
+
+	rts
+
+
+DrawBlackAndWhiteTiles
+	; Intermediate line
+	pause 64-6-1-4-3
+	move.w #$333,(a6)             ; 12/3
+	pause 128-3
+	move.w #$000,(a6)             ; 12/3
+	pause 114
+
+	; Shifting
+	moveq #16,d0
+	add.l d5,d0
+	lsl.l d0,d0                   ; 6+2n
+
+	; 16 lines of alternative black and white squares
+	move.w #16-1,d7
 .loop_squares
 	REPT 15
 	move.w d6,(a6)                ;    8/2   background color change
@@ -187,23 +256,15 @@ MonoSlide
 	swap d6                       ;    4/1
 	nop                           ;    4/1
 	ENDR                          ; = 32/8
-
 	pause 8-3
-	dbra d2,.loop_squares          ; 12/3 if branches / 16/4 if not taken
+	dbra d7,.loop_squares          ; 12/3 if branches / 16/4 if not taken
 
-	; Intermediate line
+	; Next black line
 	move.w #$000,(a6)             ; 12/3
-	pause 128-3
-	move.w #$333,(a6)             ; 12/3
-	pause 128-3
-	move.w #$000,(a6)             ; 12/3
-	pause 128-3-1-2-2-1-1-4
+	pause 57+3
+	rts             ; 16/4
 
-	lsl.w d5,d0                   ; 6+2n
 
-	dbra d1,.loop_lines 
-	dbra d3,.loop_alternate	
-	bra loop
 
 
 
@@ -241,50 +302,46 @@ MindBender
 	nop
 
 	dbra d1,.loop_lines 
-	bra loop
-
-exit
-
- ;
- ; Restore system
- ;
- ifne enable_proper_setup
-	move.w #$2700,sr
-
-	moveq #0,d0
-	move.b save_resol,d0
-	move.w d0,-(sp)
-	pea -1.w
-	pea -1.w
-	move.w #5,-(sp)
-	trap #14
-	lea 12(sp),sp
-
-	move.b save_iera,$fffffa07.w
-	move.b save_ierb,$fffffa09.w
-	move.b save_resol,$ffff8260.w
-
-	move.w #$700,$ffff8242.w
-	move.w #$000,$ffff8244.w
-	move.w #$000,$ffff8246.w
-
-	move.w #$777,$ffff8240.w
-
-	move.w #$2300,sr
 	rts
- endc
 
 
- 
- ifne enable_proper_setup
+YmData
+ dc.b 0
+ dc.b 0                ; Channel A frequency (low byte)
+
+ dc.b 1
+YmDataAFrequency
+ dc.b 0                ; Channel A frequency (high byte)
+
+ dc.b 6
+YmDataNoiseFrequency 
+ dc.b 0                ; Noise generator frequency
+
+ dc.b 7,%11110110      ; Enable Channel A tone and noise
+
+ dc.b 8
+YmDataAVolume
+ dc.b 15             ; Channel A volume
+
 
 	SECTION BSS
+
+save_70      		ds.l 1	; VBL handler
+
+save_background     ds.w 1 
 
 save_iera			ds.b 1	; Interrupt enable register A
 save_ierb			ds.b 1	; Interrupt enable register B
 save_resol			ds.b 1	; Screen resolution
+save_screen_addr_1 	ds.b 1
+save_screen_addr_2 	ds.b 1
 
- endc
+	even
+
+
+Whatever	 ds.l 1
+screen_buffer       ds.l (256+32000)/4
+
 
  end
 
