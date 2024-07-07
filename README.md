952b72b09d913cf2e663db69abf5e39dc93e8e26
Dbug <mike@defence-force.org>
2024-07-07 09:52:43 +0200

    Added the missing files and nfo updates.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Sommarhack-2024
-For the Sommarhack2024 demo party "Zero Bitplan" compo
+```
+  _____  _____  _____  _____  ______  ________    _____  _______              
+_/ ____\/ ____\/ ____\/ ____\/  __  \ \_____  \  /  |  | \   _  \    __  _  __
+\   __\\   __\\   __\\   __\ >      <  /  ____/ /   |  |_/  /_\  \   \ \/ \/ /
+ |  |   |  |   |  |   |  |  /   --   \/       \/    ^   /\  \_/   \   \     / 
+ |__|   |__|   |__|   |__|  \______  /\_______ \____   |  \_____  / /\ \/\_/  
+                                   \/         \/    |__|        \/  \/        
+
+                            ----( ffff8240.w )----
+ 			     A "Zero Bitplane" demo competition entry
+                              by Defence Force
+                             at Sommarhack 2024
+```
+
+Credits:
+========
+* Dbug..................... Pretty much everything except
+* Zerkman.................. Top of screen stabilization
+* AnCool................... Music
+
+Compatibility:
+==============
+This demo should work fine on any ATARI ST, STE or Mega STE machine with at least half a meg of ram, running at 8mhz.
+
+Results:
+========
+I made it last place \o/
+1. 4.83 points - "(n)0 PIXELS (n)0 REGRETS" by Dead Hackers Society 
+2. 4.75 points - "A Series of Raster Effects" by SMFX
+3. 3.71 points - "PHF Rally 2 - Zero Bitplane" by Psycho Hacking Force
+4. 3.38 points - "bp0d" by Extream
+5. 3.26 points - "FF8240.w" by Defence Force
+
+Source code:
+============
+The entire source code is on github: https://github.com/Dhebug/Sommarhack-2024
+I did not prettify anything, it's pretty much what you got, the result of just hacking stuff together a couple hours in the evenings the week before the party.
+
+Contact:
+========
+Defence Force HQ:
+- http://www.defence-force.org
+
+Dbug:  
+- dbug@defence-force.org
--- build.bat
+++ build.bat
@@ -9,16 +9,16 @@ for /F %%a in ('"prompt $E$S & echo on & for %%b in (1) do rem"') do set "ESC=%%
 echo %ESC%[1mBuild started: %date% %time%%ESC%[0m
 
 md export
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\dbug_surprise.png export\surprise.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\top_banner.png export\top_banner.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\made_in_5_days.png export\made_in_5_days.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\credits.png export\credits.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\greetings.png export\greetings.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\title.png export\title.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\mind_bender.png export\mind_bender.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\mono_slide.png export\mono_slide.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\slide_show.png export\slide_show.bin
-%osdk%\bin\PictConv.exe -m1 -f3 -o2 data\the_end.png export\the_end.bin
+bin\PictConv.exe -m1 -f3 -o2 data\dbug_surprise.png export\surprise.bin
+bin\PictConv.exe -m1 -f3 -o2 data\top_banner.png export\top_banner.bin
+bin\PictConv.exe -m1 -f3 -o2 data\made_in_5_days.png export\made_in_5_days.bin
+bin\PictConv.exe -m1 -f3 -o2 data\credits.png export\credits.bin
+bin\PictConv.exe -m1 -f3 -o2 data\greetings.png export\greetings.bin
+bin\PictConv.exe -m1 -f3 -o2 data\title.png export\title.bin
+bin\PictConv.exe -m1 -f3 -o2 data\mind_bender.png export\mind_bender.bin
+bin\PictConv.exe -m1 -f3 -o2 data\mono_slide.png export\mono_slide.bin
+bin\PictConv.exe -m1 -f3 -o2 data\slide_show.png export\slide_show.bin
+bin\PictConv.exe -m1 -f3 -o2 data\the_end.png export\the_end.bin
 
 ::
 :: http://sun.hasenbraten.de/vasm/index.php?view=tutorial
@@ -31,25 +31,11 @@ del final\1Bitplan.prg
 bin\vasm.exe -m68000 -Ftos -noesc -no-opt -o 0Bitplan.prg ZeroBitplan.s
 IF ERRORLEVEL 1 GOTO ErrorVasm
 
+:: Copy to the emulator folder
 copy 0Bitplan.prg D:\_emul_\atari\_mount_\DEFENCEF.RCE\0Bitplan
-::bin\upx-3.91 --ultra-brute 0Bitplan.prg -o final\1Bitplan.prg    :: 36 seconds depacking time!!!!
 
+:: Copy to the SD Card for the UltraSatan is available
 if exist S:\sommarhack\0Bitplan.prg copy 0Bitplan.prg S:\sommarhack\0Bitplan.prg
-::if exist S:\sommarhack\0Bitplan.prg copy final\1Bitplan.prg S:\sommarhack\1Bitplan.prg
-
-::bin\upx-3.91 -1 0Bitplan.prg -o final\bbdr_1.prg
-::bin\upx-3.91 -2 0Bitplan.prg -o final\bbdr_2.prg
-::bin\upx-3.91 -3 0Bitplan.prg -o final\bbdr_3.prg
-::bin\upx-3.91 -4 0Bitplan.prg -o final\bbdr_4.prg
-::bin\upx-3.91 -5 0Bitplan.prg -o final\bbdr_5.prg
-::bin\upx-3.91 -6 0Bitplan.prg -o final\bbdr_6.prg
-::bin\upx-3.91 -7 0Bitplan.prg -o final\bbdr_7.prg
-::bin\upx-3.91 -8 0Bitplan.prg -o final\bbdr_8.prg
-::bin\upx-3.91 -9 0Bitplan.prg -o final\bbdr_9.prg
-::bin\upx-3.91 -9 0Bitplan.prg -o final\bbdr_9.prg
-::bin\upx-3.91 --brute 0Bitplan.prg -o final\bbdr_brute.prg
-::bin\upx-3.91 --ultra-brute 0Bitplan.prg -o final\bbdr_ultra.prg
-
 
 ::pause
 goto :End
